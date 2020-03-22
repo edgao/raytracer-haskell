137970871049910ddb2abbd09a444e5d7a101c26
Edward Gao <edward.gao@berkeley.edu>
2020-03-22 12:10:28 -0700

    unqualified import

--- src/Vector.hs
+++ src/Vector.hs
@@ -2,29 +2,29 @@
 -- but I don't want to figure out how to use it :P
 module Vector where
 
-import qualified Linear.V3                     as V
+import Linear.V3 (V3 (..))
 
-dot :: V.V3 Double -> V.V3 Double -> Double
-dot (V.V3 x1 y1 z1) (V.V3 x2 y2 z2) = x1 * x2 + y1 * y2 + z1 * z2
+dot :: V3 Double -> V3 Double -> Double
+dot (V3 x1 y1 z1) (V3 x2 y2 z2) = x1 * x2 + y1 * y2 + z1 * z2
 (.*.) = dot
 
-scale :: Double -> V.V3 Double -> V.V3 Double
-scale scalar (V.V3 x y z) = V.V3 (scalar * x) (scalar * y) (scalar * z)
+scale :: Double -> V3 Double -> V3 Double
+scale scalar (V3 x y z) = V3 (scalar * x) (scalar * y) (scalar * z)
 (***) = scale
 
-add :: V.V3 Double -> V.V3 Double -> V.V3 Double
-add (V.V3 x1 y1 z1) (V.V3 x2 y2 z2) = V.V3 (x1 + x2) (y1 + y2) (z1 + z2)
+add :: V3 Double -> V3 Double -> V3 Double
+add (V3 x1 y1 z1) (V3 x2 y2 z2) = V3 (x1 + x2) (y1 + y2) (z1 + z2)
 (+*+) = add
 
-subtract :: V.V3 Double -> V.V3 Double -> V.V3 Double
-subtract (V.V3 x1 y1 z1) (V.V3 x2 y2 z2) = V.V3 (x1 - x2) (y1 - y2) (z1 - z2)
+subtract :: V3 Double -> V3 Double -> V3 Double
+subtract (V3 x1 y1 z1) (V3 x2 y2 z2) = V3 (x1 - x2) (y1 - y2) (z1 - z2)
 (-*-) = Vector.subtract
 
-norm :: V.V3 Double -> Double
-norm (V.V3 x y z) = sqrt (x * x + y * y + z * z)
+norm :: V3 Double -> Double
+norm (V3 x y z) = sqrt (x * x + y * y + z * z)
 
-normalize :: V.V3 Double -> V.V3 Double
+normalize :: V3 Double -> V3 Double
 normalize v = scale (1 / norm v) v
 
-reflect :: V.V3 Double -> V.V3 Double -> V.V3 Double
+reflect :: V3 Double -> V3 Double -> V3 Double
 reflect incidentRay normalVector = incidentRay -*- ((2 * (incidentRay .*. normalVector)) *** normalVector)
