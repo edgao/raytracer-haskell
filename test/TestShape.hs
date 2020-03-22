3ee776cc0c4d21a7e83ebf47a3be5e33a9d0fed4
Edward Gao <edward.gao@berkeley.edu>
2020-03-22 10:06:17 -0700

    more basic tests

--- test/TestShape.hs
+++ test/TestShape.hs
@@ -17,6 +17,15 @@ triangleTests = testGroup "Triangle" [
     testCase "Basic intersection" $ assertEqual ""
       (Just (V3 0 0 0))
       (intersect (Triangle (V3 (-1) 0 0) (V3 0 1 0) (V3 1 0 0)) (V3 0 0 1) (V3 0 0 (-1))),
+    testCase "Basic non-intersection" $ assertEqual  ""
+      Nothing
+      (intersect (Triangle (V3 (-1) 0 0) (V3 0 1 0) (V3 1 0 0)) (V3 2 2 1) (V3 0 0 (-1))),
+    testCase "Basic parallel" $ assertEqual ""
+      Nothing
+      (intersect (Triangle (V3 (-1) 0 0) (V3 0 1 0) (V3 1 0 0)) (V3 2 2 1) (V3 1 1 0)),
+    testCase "Basic line intersection" $ assertEqual ""
+      Nothing
+      (intersect (Triangle (V3 (-1) 0 0) (V3 0 1 0) (V3 1 0 0)) (V3 0 0 1) (V3 1 1 0)),
     testCase "Calculate normal" $ assertEqual ""
       (V3 0 0 1)
       (normal (Triangle (V3 (-1) 0 0) (V3 0 1 0) (V3 1 0 0)) (V3 42 84 126))
