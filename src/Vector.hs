6cf14792859cc1517b21669eaa4888aa093c7a3a
Edward Gao <edward.gao@berkeley.edu>
2020-03-22 09:19:33 -0700

    define infix operators on vectors

--- src/Vector.hs
+++ src/Vector.hs
@@ -6,12 +6,19 @@ import qualified Linear.V3                     as V
 
 dot :: V.V3 Double -> V.V3 Double -> Double
 dot (V.V3 x1 y1 z1) (V.V3 x2 y2 z2) = x1 * x2 + y1 * y2 + z1 * z2
+(.*.) = dot
 
 scale :: Double -> V.V3 Double -> V.V3 Double
 scale scalar (V.V3 x y z) = V.V3 (scalar * x) (scalar * y) (scalar * z)
+(***) = scale
 
 add :: V.V3 Double -> V.V3 Double -> V.V3 Double
 add (V.V3 x1 y1 z1) (V.V3 x2 y2 z2) = V.V3 (x1 + x2) (y1 + y2) (z1 + z2)
+(+*+) = add
+
+subtract :: V.V3 Double -> V.V3 Double -> V.V3 Double
+subtract (V.V3 x1 y1 z1) (V.V3 x2 y2 z2) = V.V3 (x1 - x2) (y1 - y2) (z1 - z2)
+(-*-) = Vector.subtract
 
 norm :: V.V3 Double -> Double
 norm (V.V3 x y z) = sqrt (x * x + y * y + z * z)
@@ -20,4 +27,4 @@ normalize :: V.V3 Double -> V.V3 Double
 normalize v = scale (1 / norm v) v
 
 reflect :: V.V3 Double -> V.V3 Double -> V.V3 Double
-reflect incidentRay normalVector = add incidentRay (scale (-2 * dot incidentRay normalVector) normalVector)
+reflect incidentRay normalVector = incidentRay -*- ((2 * (incidentRay .*. normalVector)) *** normalVector)
--- test/TestVector.hs
+++ test/TestVector.hs
@@ -11,9 +11,10 @@ import Linear.V3
 import Vector as V
 
 unitTests = testGroup "Vector unit tests" [
-    testCase "Dot product" $ assertEqual "" 32 (V.dot (V3 1 2 3) (V3 4 5 6)),
-    testCase "Scale" $ assertEqual "" (V3 2 4 6) (V.scale 2 (V3 1 2 3)),
-    testCase "Add" $ assertEqual "" (V3 5 7 9) (V.add (V3 1 2 3) (V3 4 5 6)),
+    testCase "Dot product" $ assertEqual "" 32 ((V3 1 2 3) .*. (V3 4 5 6)),
+    testCase "Scale" $ assertEqual "" (V3 2 4 6) (2 *** (V3 1 2 3)),
+    testCase "Add" $ assertEqual "" (V3 5 7 9) ((V3 1 2 3) +*+ (V3 4 5 6)),
+    testCase "Subtract" $ assertEqual "" (V3 3 3 3) ((V3 4 5 6) -*- (V3 1 2 3)),
     testCase "Norm" $ assertEqual "" 7 (V.norm (V3 2 3 6)),
     testCase "Normalize" $ assertVecEqual ""  0.0001 (V3 (2/7) (3/7) (6/7)) (V.normalize (V3 2 3 6)),
     testCase "Simple reflection" $ assertVecEqual "" 0.0001
