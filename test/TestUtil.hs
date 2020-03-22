47f29166e7ccf1d5d250af5eaee77e2329d191d6
Edward Gao <edward.gao@berkeley.edu>
2020-03-22 08:19:42 -0700

    move threshold param for readability

--- test/TestUtil.hs
+++ test/TestUtil.hs
@@ -8,8 +8,8 @@ import Control.Monad (unless)
 import Linear.V3
 
 -- A slightly tweaked version of assertEquals, which accepts a margin of error (to handle rounding error)
-assertVecEqual :: String -> V3 Double -> V3 Double -> Double -> Assertion
-assertVecEqual preface expected@(V3 ex ey ez) actual@(V3 ax ay az) threshold = 
+assertVecEqual :: String -> Double -> V3 Double -> V3 Double -> Assertion
+assertVecEqual preface threshold expected@(V3 ex ey ez) actual@(V3 ax ay az) = 
   unless (abs(ex - ax) < threshold && abs(ey - ay) < threshold && abs(ez - az) < threshold) (assertFailure msg)
   where msg = (if null preface then "" else preface ++ "\n") ++
              "expected: " ++ show expected ++ "\n but got: " ++ show actual
--- test/TestVector.hs
+++ test/TestVector.hs
@@ -16,17 +16,14 @@ unitTests = testGroup
     testCase "Scale" $ assertEqual "" (V3 2 4 6) (V.scale 2 (V3 1 2 3)),
     testCase "Add" $ assertEqual "" (V3 5 7 9) (V.add (V3 1 2 3) (V3 4 5 6)),
     testCase "Norm" $ assertEqual "" 7 (V.norm (V3 2 3 6)),
-    testCase "Normalize" $ assertVecEqual "" (V3 (2/7) (3/7) (6/7)) (V.normalize (V3 2 3 6)) 0.0001,
-    testCase "Simple reflection" $ assertVecEqual ""
+    testCase "Normalize" $ assertVecEqual ""  0.0001 (V3 (2/7) (3/7) (6/7)) (V.normalize (V3 2 3 6)),
+    testCase "Simple reflection" $ assertVecEqual "" 0.0001
       (V3 0.57735 0.57735 0.57735)
-      (V.reflect (V.normalize (V3 1 1 (-1))) (V3 0 0 1))
-      0.0001,
-    testCase "Angled reflection 1" $ assertVecEqual ""
+      (V.reflect (V.normalize (V3 1 1 (-1))) (V3 0 0 1)),
+    testCase "Angled reflection 1" $ assertVecEqual "" 0.0001
       (V3 0.2357 0.2357  0.942809)
-      (V.reflect (V.normalize (V3 (-1) (-1) 0)) (V.normalize (V3 1 1 1)))
-      0.0001,
-    testCase "Angled reflection 2" $ assertVecEqual ""
+      (V.reflect (V.normalize (V3 (-1) (-1) 0)) (V.normalize (V3 1 1 1))),
+    testCase "Angled reflection 2" $ assertVecEqual "" 0.0001
       (V3 0.4444 0.4444 0.7777)
       (V.reflect (V.normalize (V3 (-2) (-2) (-1))) (V.normalize (V3 1 1 1)))
-      0.0001
   ]
