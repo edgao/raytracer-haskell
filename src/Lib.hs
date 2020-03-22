2be5d32a6dbf4a92dfb1eea5dc4ca6f8dad18533
Edward Gao <edward.gao@berkeley.edu>
2020-03-21 21:48:17 -0700

    method was moved to Vector module

--- src/Lib.hs
+++ src/Lib.hs
@@ -8,9 +8,6 @@ import qualified Vector as V
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
 
-dot :: V3 Double -> V3 Double -> Double
-dot (V3 x1 y1 z1) (V3 x2 y2 z2) = x1 * x2 + y1 * y2 + z1 * z2
-
 data Shape = Triangle {a :: V3 Double, b :: V3 Double, c :: V3 Double}
            | Ellipsoid {center :: V3 Double, r1 :: V3 Double, r2 :: V3 Double, r3 :: V3 Double}
 
