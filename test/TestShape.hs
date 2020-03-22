08bb4708db0ac7819be79f28a291f1933da90cef
Edward Gao <edward.gao@berkeley.edu>
2020-03-22 11:40:00 -0700

    more tests

--- test/TestShape.hs
+++ test/TestShape.hs
@@ -6,6 +6,7 @@ import Test.Tasty (defaultMain, testGroup)
 import Test.Tasty.HUnit (assertEqual, testCase)
 
 import Linear.V3 (V3 (..))
+import qualified Vector as V
 
 import Shape as S
 
@@ -25,8 +26,26 @@ triangleTests = testGroup "Triangle" [
       (intersect (Triangle (V3 (-1) 0 0) (V3 0 1 0) (V3 1 0 0)) (V3 2 2 1) (V3 1 1 0)),
     testCase "Basic line intersection" $ assertEqual ""
       Nothing
-      (intersect (Triangle (V3 (-1) 0 0) (V3 0 1 0) (V3 1 0 0)) (V3 0 0 1) (V3 1 1 0)),
-    testCase "Calculate normal" $ assertEqual ""
+      (intersect (Triangle (V3 0 1 0) (V3 1 0 0) (V3 0 0 1)) (V3 0 0 1) (V3 1 1 0)),
+    testCase "Basic normal" $ assertEqual ""
       (V3 0 0 1)
-      (normal (Triangle (V3 (-1) 0 0) (V3 0 1 0) (V3 1 0 0)) (V3 42 84 126))
+      (normal (Triangle (V3 (-1) 0 0) (V3 0 1 0) (V3 1 0 0)) (V3 42 84 126)),
+    testCase "Slanted edge intersection" $ assertEqual ""
+      (Just (V3 0.5 0.5 0))
+      (intersect (Triangle (V3 0 1 0) (V3 1 0 0) (V3 0 0 1)) (V3 0 0 0) (V3 1 1 0)),
+    testCase "Slanted corner intersection" $ assertEqual ""
+      (Just (V3 1 0 0))
+      (intersect (Triangle (V3 0 1 0) (V3 1 0 0) (V3 0 0 1)) (V3 0 0 0) (V3 1 0 0)),
+    testCase "Slanted non-intersection" $ assertEqual ""
+      Nothing
+      (intersect (Triangle (V3 0 1 0) (V3 1 0 0) (V3 0 0 1)) (V3 4 4 1) (V3 1 1 1)),
+    testCase "Slanted parallel" $ assertEqual ""
+      Nothing
+      (intersect (Triangle (V3 0 1 0) (V3 1 0 0) (V3 0 0 1)) (V3 1 1 1) (V3 1 1 (-1))),
+    testCase "Slanted line intersection" $ assertEqual ""
+      Nothing
+      (intersect (Triangle (V3 0 1 0) (V3 1 0 0) (V3 0 0 1)) (V3 0 0 1) (V3 1 1 (-1))),
+    testCase "Slanted normal" $ assertEqual ""
+      (V.normalize (V3 1 1 1))
+      (normal (Triangle (V3 0 1 0) (V3 1 0 0) (V3 0 0 1)) (V3 42 84 126))
   ]
