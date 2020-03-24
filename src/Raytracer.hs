b9aee2d234f965700f7a1338cc0e0cb6517a6c4f
Edward Gao <edward.gao@berkeley.edu>
2020-03-23 19:56:22 -0700

    reorganize

--- src/Raytracer.hs
+++ src/Raytracer.hs
@@ -1,5 +1,5 @@
 module Raytracer
-    (
+    ( trace
     ) where
 
 import qualified Shape as S
@@ -9,12 +9,20 @@ import Linear.V3 (V3 (..))
 import Vector ((.*.), (***), (+*+), (-*-))
 
 trace :: [(S.Shape, M.Material)] -> [M.Light] -> M.Color -> V.Ray -> M.ReflectionStrategy -> Int -> M.Color
-trace shapes lights ambientLight (V.Ray origin direction) _ 0 =
+trace shapes lights ambientLight ray@(V.Ray origin direction) _ 0 =
+  case findIntersection shapes ray of
+    Nothing -> M.Color 0 0 0
+    Just (intersection, normal, material) -> M.reflectedLight material lights ambientLight normal origin intersection
+trace shapes lights ambientLight ray@(V.Ray origin direction) reflectionStrategy maxBounces =
+  M.Color 0 0 0
+
+findIntersection :: [(S.Shape, M.Material)] -> V.Ray -> Maybe (V3 Double, V3 Double, M.Material)
+findIntersection shapes ray@(V.Ray origin direction) = 
   let intersections = map extract $ filter isPresent $ map getData shapes
-  in if null intersections then M.Color 0 0 0
+  in if null intersections then Nothing
      else let (firstIntersectionData:rest) = map getRatio intersections
-              (intersectionPoint, normal, material, _) = foldr selectLower firstIntersectionData rest
-          in M.reflectedLight material lights ambientLight normal origin intersectionPoint
+              (intersection, normal, material, _) = foldr selectLower firstIntersectionData rest
+          in Just (intersection, normal, material)
   where getData (shape, material) =
           let intersectData = S.intersectData shape ray
           in case intersectData of
@@ -27,5 +35,3 @@ trace shapes lights ambientLight (V.Ray origin direction) _ 0 =
         vecDiv (V3 x1 _ _ ) (V3 x2 _ _) = x1 / x2
         getRatio (point, normal, material) = (point, normal, material, vecDiv (normal -*- origin) direction)
         selectLower i1@(int1, norm1, mat1, ratio1) i2@(int2, norm2, mat2, ratio2) = if ratio1 < ratio2 then i1 else i2
-trace shapes lights ambientLight ray@(V.Ray origin direction) reflectionStrategy maxBounces =
-  Color 0 0 0
