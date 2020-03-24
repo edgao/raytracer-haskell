84407de5b50c8e635862164ba8481abfc4ccf3fb
Edward Gao <edward.gao@berkeley.edu>
2020-03-23 20:43:51 -0700

    create raytracer harness

--- app/PhongBak.hs
+++ app/PhongBak.hs
@@ -1,31 +1,29 @@
-module Main where
+module PhongBak where
 
 import qualified Data.ByteString.Lazy as BS (writeFile)
 import Codec.Picture as P (PixelRGB8 (..), DynamicImage (ImageRGB8), generateImage)
 import Codec.Picture.Saving as PS (imageToPng)
-import Debug.Trace (trace)
 
 import qualified Material as M
-import qualified Shape as S
-import qualified Raytracer as R
 import Linear.V3 (V3 (..))
 import qualified Vector as V
-import Vector ((-*-))
 
 main :: IO ()
 main = do
-  let width = 100
-  let height = 100
-  let camera = V3 0 0 10
-  let
+  let width = 1000
+  let height = 1000
+  let 
     fn :: Int -> Int -> PixelRGB8
-    fn imgX imgY =
-        let x = 4.0 * (fromIntegral imgX) / (fromIntegral width) - 2
+    fn imgX imgY = 
+        let x :: Double
+            x = 4.0 * (fromIntegral imgX) / (fromIntegral width) - 2
+            y :: Double
             y = 2 - 4.0 * (fromIntegral imgY) / (fromIntegral height)
             intersection = sphereIntersection x y
-            viewport = V3 x y 5
-            (r, g, b) = M.render $ M.clampColor $ R.trace [(shape, material)] lights ambientLight reflectionStrategy 1 (V.Ray camera (V.normalize $ viewport - camera))
-        in trace ("Calculated (" ++ show imgX ++ ", " ++ show imgY ++ ") to be " ++ show (r, g, b)) $ P.PixelRGB8 (fromIntegral r) (fromIntegral g) (fromIntegral b)
+        in case intersection of
+            Nothing -> P.PixelRGB8 0 0 0
+            Just location -> P.PixelRGB8 (fromIntegral r) (fromIntegral g) (fromIntegral b)
+              where (r, g, b) = M.render $ M.clampColor $ M.reflectedLight material lights ambientLight (V.normalize location) (V3 x y 10) location
   let img = P.generateImage fn width height
   BS.writeFile "/home/edgao/Desktop/test.png" $ PS.imageToPng $ P.ImageRGB8 img
   return ()
@@ -36,14 +34,12 @@ sphereIntersection x y = if x * x + y * y > 1 then Nothing
 
 material = M.PhongMaterial (M.Color 0.2 0 0) (M.Color 0 0.7 0) (M.Color 0 0 1) 10
 
-shape = S.Triangle (V3 1 1 0) (V3 4 4 0) (V3 8 1 0)
-
 lights = [
-    M.Light
+    (
+      M.Light
       (M.Color 1 1 1)
       (V3 100 100 100)
+    )
   ]
 
 ambientLight = M.Color 1 1 1
-
-reflectionStrategy = M.Mirror
